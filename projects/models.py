c8e4a2859700b7bacfd17643c146c91758d1d6f4
willjw3 <will.j.ward3@gmail.com>
2021-09-28 12:36:12 +0900

    Added Board and Task models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    name = models.CharField(max_length=100)
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
