5d5df44a6d49d51f45bcd799ae1618ed49b6e12e
willjw3 <will.j.ward3@gmail.com>
2021-09-28 14:43:01 +0900

    Added 'users' app

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Board, Task, Comment
 
-# Register your models here.
+admin.site.register(Board)
+admin.site.register(Task)
+admin.site.register(Comment)
+
