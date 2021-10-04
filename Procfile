37b472d0ae1933f27c882e768a0a90e36abd9300
willjw3 <will.j.ward3@gmail.com>
2021-10-04 13:27:45 +0900

    Added Procfile and updated settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kanri.wsgi:application --log-file -
--- kanri/settings.py
+++ kanri/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
+import environ, os
 
 # Initialize environment variables
 env = environ.Env()
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KI')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
